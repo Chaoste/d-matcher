b98ccade18e8269d3525b0d1783a47a9d6175742
Thomas Kellermeier <t-kellermeier@hotmail.de>
2018-08-20 00:38:22 +0200

    Timestamp output

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 from typing import Optional, Tuple, Union
+from time import gmtime, strftime
 
 import pandas as pd
 import numpy as np
 
 import algorithms as algo
 import utils
-import metrics
+# import metrics
 
 METRICS = ('Gender', 'Discipline', 'Nationality', 'Collision')
 SEMESTER = ('WT-15', 'ST-16', 'WT-16', 'ST-17')
@@ -34,7 +35,9 @@ def find_teaming(students: pd.DataFrame,
 
 def store_output(teaming: Teaming, path: str):
     _, filename = os.path.split(path)
-    output_path = os.path.join('output', 'teamings_{}'.format(filename))
+    filename, ext = os.path.splitext(filename)
+    timestamp = strftime("%Y_%m_%d_%H_%M_%S", gmtime())
+    output_path = os.path.join('output', f'teamings_{timestamp}.{ext}')
     utils.store_teaming(teaming, output_path)
     print('Saved results to {}.'.format(output_path))
 
